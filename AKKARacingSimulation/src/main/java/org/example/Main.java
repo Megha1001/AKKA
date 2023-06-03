b53fe9d5d30ab4aa70b7b91497a3592e675fde95
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2023-06-03 20:33:28 +0530

    Add files via upload

---
+++ AKKARacingSimulation/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import akka.actor.typed.ActorSystem;
+
+public class Main {
+    public static void main(String[] args) {
+        ActorSystem<RaceController.Command>raceController = ActorSystem.create(RaceController.create(),"RaceSimulation");
+        raceController.tell(new RaceController.StartCommand());
+    }
+}
